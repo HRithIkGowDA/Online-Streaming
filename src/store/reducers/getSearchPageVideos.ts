ca95d7faee97d05551cac7b94b05360584aba050
viru <38450997+HRithIkGowDA@users.noreply.github.com>
2023-01-10 19:15:34 +0530

    yt

---
+++ src/store/reducers/getSearchPageVideos.ts
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { RootState } from "..";
+import { HomePageVideos } from "../../Types";
+import { parseData } from "../../utils";
+import { YOUTUBE_API_URL } from "../../utils/constants";
+
+const API_KEY = "AIzaSyD_CxtELVPVfGOoQxAw1E_uhb47rd99fzk";
+
+export const getSearchPageVideos = createAsyncThunk(
+  "youtubeApp/serachPageVideos",
+  async (isNext: boolean, { getState }) => {
+    const {
+      youtubeApp: { nextPageToken: nextPageTokenFromState, videos, searchTerm },
+    } = getState() as RootState;
+    const {
+      data: { items, nextPageToken },
+    } = await axios.get(
+      `${YOUTUBE_API_URL}/search?q=${searchTerm}&key=${API_KEY}&part=snippet&type=video&${
+        isNext ? `pageToken=${nextPageTokenFromState}` : ""
+      }`
+    );
+    const parsedData: HomePageVideos[] = await parseData(items);
+    return { parsedData: [...videos, ...parsedData], nextPageToken };
+  }
+);
